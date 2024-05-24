24bf8daf853feb79351a0b4d67800c391f9559ff
GillianCharra <gillian@charra.fr>
2024-05-24 14:15:31 +0200

    l'heure de prier

--- src/main/java/com/example/catalogue/entities/Location.java
+++ src/main/java/com/example/catalogue/entities/Location.java
@@ -5,27 +5,23 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
 @Document
 public class Location {
-    @Getter
     @Setter
     @Id
     private String id;
 
-    @Getter
     @Setter
     private String clientId;
 
-    @Getter
     @Setter
     private String articleId;
 
-    @Getter
     @Setter
     private String startDate;
 
 
-    @Getter
     @Setter
     private int duration;
 
